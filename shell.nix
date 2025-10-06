dd0348a78c15b2fe9b20fd161a89c371d968372b
Adamocho <81510507+Adamocho@users.noreply.github.com>
2025-10-06 13:05:39 +0200

    Add sha256 for the github package (simulate a flake)

--- shell.nix
+++ shell.nix
@@ -1,11 +1,16 @@
 let
-  rust-overlay = builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
+  rust-overlay = builtins.fetchTarball {
+    url = "https://github.com/oxalica/rust-overlay/archive/refs/tags/snapshot/2025-01-11.tar.gz";
+    sha256 = "0p8qjk100jics1y4zqffkwy1crwz78ia9ilypaasfv94qm9jdpwa";
+  };
 
   pkgs = import <nixpkgs> {
     overlays = [ (import rust-overlay) ];
   };
 
+  # Impossible to add a sha256 due to the design of the script
   toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+
 in
 pkgs.mkShell rec {
   packages = [ toolchain ];
